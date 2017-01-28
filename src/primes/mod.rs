a1d766b0b0e3f1746e3acb62170f4a4ec25e46cd
James Elford <james.p.elford@gmail.com>
2017-01-28 15:41:03 +0000

    Improve the performance of stream of primes by:
    * Only using a segmented seive where the size of the seive might justify
    * When adding new seive segments, only look at prime seeds that could
    	 affect this segment, so that time grows with sqrt(prime list)
    	 rather than linear(prime list)

--- src/primes/mod.rs
+++ src/primes/mod.rs
@@ -2,6 +2,7 @@
 
 pub struct StreamOfPrimes {
     seed_primes: Vec<u64>,
+    upper_bound: u64,
     current_index: usize,
     segment_size: usize,
     next_segment_floor: u64,
@@ -11,7 +12,7 @@ fn primes_from_next_seive_segment(floor: u64, size: usize, seed_primes: &Vec<u64
     let mut result = Vec::with_capacity(size / 2);
 
     let mut new_segment = vec![true; size+1];
-    for i in seed_primes.iter() {
+    for i in seed_primes.iter().take_while(|&x| x.pow(2) < (floor + size as u64)) {
         let p = i.clone() as f64;
 
         let start_at = (floor as f64 / p).ceil() as u64;
@@ -40,8 +41,9 @@ impl Iterator for StreamOfPrimes {
             let current_index = self.current_index;
             self.current_index += 1;
             Some(self.seed_primes[current_index])
+        } else if self.next_segment_floor >= self.upper_bound {
+            None
         } else {
-
             let mut next_segment = primes_from_next_seive_segment(self.next_segment_floor,
                                                                   self.segment_size,
                                                                   &self.seed_primes);
@@ -55,10 +57,13 @@ impl Iterator for StreamOfPrimes {
     }
 }
 
-
-
 pub fn stream_of_primes(upper_bound: u64) -> StreamOfPrimes {
-    let segment_size = (upper_bound as f64).sqrt().ceil() as usize;
+
+    let segment_size = if upper_bound > 1000 {
+        (upper_bound as f64).sqrt().ceil() as usize
+    } else {
+        (upper_bound + 1) as usize
+    };
 
     let mut initial_seive = vec![true; segment_size + 1];
     initial_seive[0] = false;
@@ -68,7 +73,7 @@ pub fn stream_of_primes(upper_bound: u64) -> StreamOfPrimes {
 
     for i in 2..segment_size {
         if initial_seive[i] {
-            for j in i..(segment_size / i) {
+            for j in i..(segment_size / i) + 1 {
                 initial_seive[i * j] = false
             }
             seed_primes.push(i as u64);
@@ -77,6 +82,7 @@ pub fn stream_of_primes(upper_bound: u64) -> StreamOfPrimes {
 
     StreamOfPrimes {
         seed_primes: seed_primes,
+        upper_bound: upper_bound,
         current_index: 0,
         segment_size: segment_size,
         next_segment_floor: segment_size as u64,
@@ -108,5 +114,13 @@ mod tests {
         assert_eq!(s.next(), Some(3));
         assert_eq!(s.next(), Some(5));
         assert_eq!(s.next(), Some(7));
+    }
+
+    #[test]
+    fn stream_of_primes_finished_at_its_upper_bound() {
+        let r = stream_of_primes(100).collect::<Vec<_>>();
+        assert_eq!(r,
+                   vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67,
+                        71, 73, 79, 83, 89, 97]);
     }
 }
